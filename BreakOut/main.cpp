133787a7e1a9a8abc30e30f65656f267a2810f49
AbyssNova <zhan657@ucsc.edu>
2019-11-22 02:13:11 -0800

    update logic

--- BreakOut/main.cpp
+++ BreakOut/main.cpp
@@ -1,18 +1,28 @@
 #include <SFML/Graphics.hpp>
-#include <SFML/Graphics.hpp>
-#include <SFML/Audio.hpp>
 #include <sstream>
 #include<string>
 #include <iostream>
 #include<vector>
+#include <string>  
+#include <vector>  
+#include <fstream>  
+#include <sstream>  
 using namespace std;
 using namespace sf;
-
+float dtAsSeconds;
 class Block
 {
 public:
 	Block() {
-
+		blockHP = 1;
+		blockType = 0;
+	}
+	int Hitted() {
+		blockHP -= 1;
+		if (blockHP == 0)
+		{
+			Destroy();
+		}
 	}
 	void Destroy() {
 
@@ -29,10 +39,10 @@ public:
 class Ball {
 public:
 	Ball();
-	void reset();
+	void start();
 	void draw(RenderWindow* win);
 	CircleShape theball;
-	int radius;
+	float radius;
 	Vector2f loc;
 	Vector2f speed;
 	Texture ballOne;
@@ -42,8 +52,8 @@ Ball::Ball() {
 	radius = 20;
 	speed.x=0;
 	speed.y = 0;
-	loc.x = 0;
-	loc.y=	0;
+	loc.x = 250 - radius;
+	loc.y = 1000 - 30 - 20 - 2*radius;
 	CircleShape theball(radius);
 	this->theball.setPosition(loc);
 	if (!ballOne.loadFromFile("images/ball.png"))
@@ -61,28 +71,46 @@ void Ball::draw(RenderWindow* win) {
 	this->theball.setTexture(&ballOne);
 	win->draw(theball);
 }
-void Ball::reset() {
-	theball.setPosition(loc);
-	theball.setRadius(radius);
-	this->theball.setTexture(&ballOne);
+void Ball::start() {
+	float spd = 100;
+	speed.x = spd*sin(0.5)* dtAsSeconds;
+	speed.y = -spd*cos(0.5) * dtAsSeconds;
 }
 
 class Paddle {
 public:
-	Paddle();
+	Paddle()
+	{
+		size.x = (100);
+		size.y = 20;
+		loc.x = 250 - size.x / 2;
+		loc.y = 1000 - 30 - size.y;
+		RectangleShape thispd;
+		thispd.setPosition(loc);
+	};
 	void draw(RenderWindow* win);
 	Vector2f size;
 	Vector2f loc;
-	Vector2f speed;
 	RectangleShape thispd;
+	void moveleft(float speed) {
+		if (loc.x- speed > 0)
+			loc.x-=speed;
+		else
+			return;
+	}
+	void moveright(float speed) {
+		if (loc.x + speed < 500 - size.x)
+			loc.x +=speed;
+		else
+			return;
+	}
+	
 };
-Paddle::Paddle() {
 
-	thispd.setSize(size);
-	thispd.setPosition(loc);
-}
 void Paddle::draw(RenderWindow* win) {
-	thispd.setPosition(Vector2f(locX, locY));
+	thispd.setPosition(loc);
+	thispd.setSize(size);
+	thispd.setFillColor(Color::Blue);
 	win->draw(thispd);
 }
 
@@ -98,8 +126,10 @@ public:
 	void renderwin(int);
 	RenderWindow gamewindow;
 	Ball ball;
-	Paddle PaddleP1;
-	Block[]
+	Paddle Paddle;
+	int gamestate = 0;// 0 for wait 1 for run 2 for end;
+	Block map[5][10][10];
+	Vector2f dloc;
 	int point;
 	//Buffrect buff;
 };
@@ -107,26 +137,27 @@ public:
 
 Game::Game() : gamewindow(sf::VideoMode(500, 1000), "PONG") {
 	Ball ball;
-	Paddle Paddle;
+	//Paddle;
+	dloc = ball.loc - Paddle.loc;
+	point = 0;
 }
 
 void Game::run() {
 	Time timer;
 	Clock clock;
-	ball.reset();
 	while (gamewindow.isOpen()) {
-		gamewindow.setFramerateLimit(80);
+		gamewindow.setFramerateLimit(288);
 		timer = clock.restart();
-		float dtAsSeconds = timer.asSeconds();
 		processEvents(dtAsSeconds);
 		update(dtAsSeconds);
 		render();
+		dtAsSeconds = timer.asSeconds();
 	}
 }
 
 void Game::processEvents(float dt) {
 	Event event;
-	float pdspeed = 600 * dt;
+	float pdspeed = 1000 * dt;
 	while (gamewindow.pollEvent(event)) {
 		if ((event.type == Event::Closed) || ((event.type == Event::KeyPressed) && (event.key.code == Keyboard::Escape))) {
 			gamewindow.close();
@@ -135,17 +166,15 @@ void Game::processEvents(float dt) {
 			if (event.key.code == Keyboard::Escape) {
 				gamewindow.close();
 			}
-			else if (event.key.code == Keyboard::Up) {
-				if (PaddleP2.locY - pdspeed > 0)
-					PaddleP2.locY -= pdspeed;
-				else
-					PaddleP2.locY = 0;
+			else if (event.key.code == Keyboard::Space) {
+				gamestate = 1;
+				ball.start();
 			}
-			else if (event.key.code == Keyboard::Down) {
-				if (PaddleP2.locY + pdspeed + PaddleP2.length < 600)
-					PaddleP2.locY += pdspeed;
-				else
-					PaddleP2.locY = 600 - PaddleP2.length;
+			else if (event.key.code == Keyboard::Left) {
+				Paddle.moveleft(pdspeed);
+			}
+			else if (event.key.code == Keyboard::Right) {
+				Paddle.moveright(pdspeed);
 			}
 
 		}
@@ -153,7 +182,12 @@ void Game::processEvents(float dt) {
 }
 
 void Game::update(float dtime) {
-
+	if (gamestate == 0) {
+		ball.loc = Paddle.loc+dloc;
+	}
+	if (gamestate == 1) {
+		ball.loc+=ball.speed;
+	}
 }
 void Game::renderwin(int n) {
 
@@ -162,17 +196,13 @@ void Game::renderwin(int n) {
 void Game::render() {
 	Text text;
 	gamewindow.clear();
-	PaddleP1.draw(&gamewindow);
-	PaddleP2.draw(&gamewindow);
-	
-
+	Paddle.draw(&gamewindow);
 	ball.draw(&gamewindow);
-
 	gamewindow.draw(text);
 	gamewindow.display();
 }
 
-int main(int argc, char* argv[]) {
+int main() {
 	Game game;
 	game.run();
 	return 0;
